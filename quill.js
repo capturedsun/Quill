cb70af8832c1a4c59b0c5ad3a7cc5ceeb25c7a57
metacryst <samrussell99@gmail.com>
2023-09-02 14:54:01 -0500

    323: Making VSCode snippets for templates

--- quill.js
+++ quill.js
@@ -1,11 +1,47 @@
 export function addStyle(cssString) {
-    let stylesheet = new CSSStyleSheet();
-    stylesheet.replaceSync(cssString)
-    document.adoptedStyleSheets = [...document.adoptedStyleSheets, stylesheet];
+  if(quill.detectMobile() || quill.getSafariVersion() < 16.4) {
+      let container = document.querySelector("style#quillStyles");
+      if(!container) {
+          container = document.createElement('style');
+          container.id = "quillStyles";
+          document.head.appendChild(container);
+      }
+
+      let primarySelector = cssString.substring(0, cssString.indexOf("{")).trim();
+      primarySelector = primarySelector.replace(/\*/g, "all");
+      let stylesheet = container.querySelector(`:scope > style#${primarySelector}`)
+      if(!stylesheet) {
+          stylesheet = document.createElement('style');
+          stylesheet.id = primarySelector;
+          stylesheet.appendChild(document.createTextNode(cssString));
+          container.appendChild(stylesheet);
+      } else {
+          stylesheet.innerText = cssString
+      }
+      return;
   }
-  
-export function html(elementString) {
-    let parser = new DOMParser();
-    let doc = parser.parseFromString(elementString, 'text/html');
-    return doc.body.firstChild;
+  let stylesheet = new CSSStyleSheet();
+  stylesheet.replaceSync(cssString)
+  document.adoptedStyleSheets = [...document.adoptedStyleSheets, stylesheet];
+}
+
+export function createElement(elementString) {
+  let parser = new DOMParser();
+  let doc = parser.parseFromString(elementString, 'text/html');
+  return doc.body.firstChild;
+}
+
+export function detectMobile() {
+  const mobileDeviceRegex = /Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i;
+  return mobileDeviceRegex.test(navigator.userAgent);
+}
+
+export function getSafariVersion() {
+  const userAgent = navigator.userAgent;
+  const isSafari = userAgent.includes("Safari") && !userAgent.includes("Chrome");
+  if (isSafari) {
+    const safariVersionMatch = userAgent.match(/Version\/(\d+\.\d+)/);
+    const safariVersion = safariVersionMatch ? parseFloat(safariVersionMatch[1]) : null;
+    return safariVersion;
+  }
 }
